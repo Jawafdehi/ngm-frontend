4bf044cba17e6157d43d9f875abc8497fd99bbd0
samyam <sjungthapa37@gmail.com>
2026-05-04 20:24:42 +0545

    fixed the proxy api target. changed it to "https://portal.jawafdehi.org/"

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://ngm-store.jawafdehi.org',
+        target: 'https://portal.jawafdehi.org/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
